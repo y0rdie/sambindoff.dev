43cfff8a15cf501e56b32be1210553810cab51ba
Sam Bindoff <sdbindoff@gmail.com>
2021-06-16 22:47:41 +0100

    Email update

--- source/_partials/icons.blade.php
+++ source/_partials/icons.blade.php
@@ -10,6 +10,6 @@
     <svg class="w-4 h-4 stroke-current text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
 </a>
 
-<a href="mailto:&#104;&#101;&#121;&#64;&#115;&#97;&#109;&#98;&#105;&#110;&#100;&#111;&#102;&#102;&#46;&#100;&#101;&#118;" title="Email" target="_blank" class="ml-2">
+<a href="mailto:&#109;&#101;&#064;&#115;&#097;&#109;&#098;&#105;&#110;&#100;&#111;&#102;&#102;&#046;&#100;&#101;&#118;" title="Email" target="_blank" class="ml-2">
     <svg class="w-4 h-4 stroke-current text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
 </a>
